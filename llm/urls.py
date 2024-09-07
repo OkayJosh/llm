b327e64c98017252a7e75fa8f4ee8a609ab94abe
Joshua Olatunji <joshua+github@etentlabs.com>
2024-09-07 17:41:50 +0100

    LLM: llm urls

---
+++ llm/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for llm project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from django_prometheus import exports
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="LLM Performance API",
+        default_version='v1',
+        description="API for fetching and ranking LLM performance metrics.",
+        terms_of_service="https://okayjosh.github.io/",
+        contact=openapi.Contact(email="joshua@etentlabs.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
+
+urlpatterns = [
+    path('api/', include('benchmarks.urls')),
+    path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('metrics/', exports.ExportToDjangoView, name="prometheus-django-metrics"),
+]
