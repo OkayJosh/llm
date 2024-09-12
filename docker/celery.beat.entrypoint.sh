04f3b1a917c9fbb1e05f1713a3561fff594a2aff
Joshua Olatunji <joshua+github@etentlabs.com>
2024-09-13 00:22:21 +0100

    LLM: updated

--- docker/celery.beat.entrypoint.sh
+++ docker/celery.beat.entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Make the database check script executable
-chmod +x ./reach_database.sh
+#chmod +x ./reach_database.sh
 
 # Navigate to the project directory
-PROJECT_DIR="$HOME/llm" # Should work like this in Docker
-PROJECT_DIR="$HOME/lycon/llm"
+PROJECT_DIR="$WORKDIR" # Should work like this in Docker
 echo "Changing to project directory: ${PROJECT_DIR}..."
 cd "${PROJECT_DIR}" || {
     echo "Failed to change to project directory. Exiting." >&2
